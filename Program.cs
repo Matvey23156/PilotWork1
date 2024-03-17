236a03cd7aafaf566a91f0728bc6b5b4a3c05bb0
Matvey23156 <151012776+Matvey23156@users.noreply.github.com>
2024-03-17 17:53:47 +1000

    add func PrintArray

--- Program.cs
+++ Program.cs
@@ -13,3 +13,11 @@ void NewArrayLine(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
